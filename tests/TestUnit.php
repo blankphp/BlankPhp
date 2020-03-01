b13d643337785fbf6ae9f509b9b567eafc2dd149
blankworld <1136589038@qq.com>
2020-03-01 22:28:20 +0800

    resolve test problem

--- tests/TestUnit.php
+++ tests/TestUnit.php
@@ -8,29 +8,16 @@ class TestUnit extends TestCase
 
     public function createApplication()
     {
-        define('APP_PATH', dirname(__DIR__));
-        $app = \Blankphp\Application::init();
-        $app->make(\Blankphp\Contract\Kernel::class);
+        define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+        $app = \Blankphp\Application::getInstance();
         return $this->app = $app;
     }
 
     public function testBasicTest()
     {
         $response = $this->get('/');
-        $res = '<style type="text/css">
-                *{ padding: 0; margin: 0;text-align: center }
-                 body{
-                  font-family: Helvetica, "Microsoft YaHei", Arial, sans-serif;
-                    font-size: 14px;
-                 color: #333}
-                  h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; }
-                  p{ line-height: 2em; font-size: 45px }</style>
-                  <div style="padding: 50px;">
-                <h1>🚀</h1><p> BlankPhp V1<br/><span style="font-size:30px">每日练习，刻意精进 <br>
-                为phpWeb开发设计的高性能框架</span>
-                </p><span style="font-size:22px;">
-                </span></div></script>
-                <blankPhp id="dadad12596"></blankPhp>';
+
+        $res = '<style type="text/css">*{ padding: 0; margin: 0;text-align: center }body{font-family: Helvetica, "Microsoft YaHei", Arial, sans-serif;font-size: 14px; color: #333}h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; }p{ line-height: 2em; font-size: 45px }</style><div style="padding: 50px;"><h1>🚀</h1><p> BlankPhp V1<br/><span style="font-size:30px">每日练习，刻意精进 <br>为phpWeb开发设计的高性能框架</span></p><span style="font-size:22px;"></span></div></script><blankPhp id="dadad12596"></blankPhp>';
         $this->assertEquals($res, $response);
     }
 
@@ -45,7 +32,6 @@ class TestUnit extends TestCase
         $this->createApplication();
         $kernel = $this->app->make(\Blankphp\Contract\Kernel::class);
         define('APP_ENV', 'testing');
-
         $response = $kernel->handle(
             \Blankphp\Request\TestRequest::create($method, $uri, $parameters, $cookies,
                 $files, $server, $content)
